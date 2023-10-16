d28c365ca03db6778330c8c2a73ffcc38ee3c760
rawatchanu <chandrawati@kasperinfotech.org>
2023-10-16 17:40:16 +0530

    file

---
+++ script.js
@@ -0,0 +1,11 @@
+let humburger=document.querySelector(".humburger");
+humburger.onclick= function(){
+let navbar=document.querySelector(".navbar");
+navbar.classList.toggle("active");
+
+}
+
+
+
+
+
